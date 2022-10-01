1ce076c3ace7905c651fee3f158da0fd53c51891
Umur Dincer <umuredipdincer@gmail.com>
2022-10-01 12:23:00 +0300

    initial commit

---
+++ 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
---
+++ 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
@@ -0,0 +1,6 @@
+ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
+ ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
+ C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+C:/Users/LENOVO/Desktop/FreeRTOS\ Learning/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
